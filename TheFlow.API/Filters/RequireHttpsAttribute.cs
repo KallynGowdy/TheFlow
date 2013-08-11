a91aa4cef54d696a09d12d004d324e939d50c233
kallyn_gowdy <kallyngowdy@gmail.com>
2013-08-11 18:49:35 -0400

    Added Dependency Injection support with ninject.

---
+++ TheFlow.API/Filters/RequireHttpsAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Filters;
+using System.Net;
+using System.Text;
+
+namespace TheFlow.API.Filters
+{
+    /// <summary>
+    /// Defines an attribute that causes Https to be required for every method that this attribute is applied to.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class RequireHttpsAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(System.Web.Http.Controllers.HttpActionContext actionContext)
+        {
+            if (!string.Equals(actionContext.Request.RequestUri.Scheme, "https", StringComparison.OrdinalIgnoreCase))
+            {
+                actionContext.Response = new HttpResponseMessage(HttpStatusCode.Forbidden)
+                {
+                    Content = new StringContent("Https (SSL) is required to access this API", Encoding.UTF8)
+                };
+            }
+        }
+    }
+}
